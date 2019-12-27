43694c8d4ec25acd50383f9df67fc5b9de010426
sandeepini <sandeepkondaiah@gmail.com>
2019-12-27 16:12:41 +0530

    Second commit

---
+++ FastTrackBTR/src/test/java/com/actitime/testscript/TestCase2.java
@@ -0,0 +1,13 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestCase2 {
+	@Test
+	public void testCreatNewTask() {
+		//Assert.fail();
+		Reporter.log("create new task",true );
+		
+	}
+}
